a1f93393b002c27caad18093ae199999be2da5cb
adityakul411 <kulkarniaditya464@gmail.com>
2024-01-03 12:03:29 +0530

    Scratched project photoshare

--- photoshare/photos/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from photos import views
-
-urlpatterns = [
-    path('', views.gallery, name="gallery"),
-    path('add/', views.addPhoto, name="add"),
-    path('photo/<str:pk>/', views.viewPhoto, name="photo"),
-]
