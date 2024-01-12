627076e6082467b8aa66f93fcbe0dae0675a3ab7
adityakul411 <kulkarniaditya464@gmail.com>
2024-01-12 11:54:34 +0530

    Added deserialization example

--- bookListAPI-DRF-Intro/myapp/serializers.py
+++ bookListAPI-DRF-Intro/myapp/serializers.py
@@ -8,9 +8,11 @@ class CategorySerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id', 'slug', 'title']
 
-class MenuSerializer(serializers.HyperlinkedModelSerializer):
+class MenuSerializer(serializers.ModelSerializer):
     stock = serializers.IntegerField(source='inventory')
     price_after_tax = serializers.SerializerMethodField(method_name='calculateTax')
+    category = CategorySerializer(read_only=True)
+    category_id = serializers.IntegerField(write_only=True)
     # category  = serializers.HyperlinkedRelatedField(
     #     queryset = Category.objects.all(),
     #     view_name='category-detail'
@@ -20,4 +22,4 @@ class MenuSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Menu
-        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category']
+        fields = ['id', 'title', 'price', 'stock', 'price_after_tax', 'category', 'category_id']
--- bookListAPI-DRF-Intro/myapp/views.py
+++ bookListAPI-DRF-Intro/myapp/views.py
@@ -1,5 +1,5 @@
 from .models import Menu, Category
-from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.shortcuts import render, get_object_or_404
@@ -7,12 +7,18 @@ from .serializers import MenuSerializer, CategorySerializer
 
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def menu(request):
-    menu = Menu.objects.select_related('category').all()
-    serialized_menu = MenuSerializer(menu, many=True, context={'request':request})
-    return Response(serialized_menu.data)
-
+    if request.method == 'GET':
+        menu = Menu.objects.select_related('category').all()
+        serialized_menu = MenuSerializer(menu, many=True, context={'request':request})
+        return Response(serialized_menu.data)
+    if request.method == 'POST':
+        # Deserialize the request data
+        serialized_menu = MenuSerializer(data=request.data)
+        serialized_menu.is_valid(raise_exception=True)
+        serialized_menu.save()
+        return Response(serialized_menu.data, status.HTTP_201_CREATED)
 
 @api_view(['GET', 'POST'])
 def item(request, id):
