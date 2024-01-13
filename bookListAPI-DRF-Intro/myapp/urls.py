74e99e3f1573a587dc2f7d53b25c8807943f6db2
adityakul411 <kulkarniaditya464@gmail.com>
2024-01-13 15:18:53 +0530

    Exploring third party renderers

--- bookListAPI-DRF-Intro/myapp/urls.py
+++ bookListAPI-DRF-Intro/myapp/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path('menu', views.menu),
+    path('menu_items', views.menu_items),
     path('item/<int:id>', views.item),
     path('category/<int:pk>',views.category_detail, name='category-detail'),
 ]
--- bookListAPI-DRF-Intro/myapp/views.py
+++ bookListAPI-DRF-Intro/myapp/views.py
@@ -1,9 +1,10 @@
 from .models import Menu, Category
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from django.shortcuts import render, get_object_or_404
+from rest_framework.renderers import TemplateHTMLRenderer
 from .serializers import MenuSerializer, CategorySerializer
+from rest_framework.decorators import api_view, renderer_classes
 
 
 # Create your views here.
@@ -20,6 +21,7 @@ def menu(request):
         serialized_menu.save()
         return Response(serialized_menu.data, status.HTTP_201_CREATED)
 
+
 @api_view(['GET', 'POST'])
 def item(request, id):
     item = get_object_or_404(Menu, pk=id)
@@ -32,3 +34,11 @@ def category_detail(request,pk):
     category = get_object_or_404(Category, pk=pk)
     serialized_category = CategorySerializer(category)
     return Response(serialized_category.data)
+
+# Trying out renderers which are not included in the default. (Default are JSONRenderer and HTMLRenderer)
+@api_view()
+@renderer_classes([TemplateHTMLRenderer])
+def menu_items(request):
+    items = Menu.objects.select_related('category').all()
+    ser_item = MenuSerializer(items, many=True)
+    return Response({'data':ser_item.data}, template_name='menu-items.html')
