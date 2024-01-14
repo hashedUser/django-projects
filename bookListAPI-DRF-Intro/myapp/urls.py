316b6b916f5ff974e7b48c6d2bc7290c5242eefa
adityakul411 <kulkarniaditya464@gmail.com>
2024-01-14 21:14:54 +0530

    Implemented user groups

--- bookListAPI-DRF-Intro/myapp/urls.py
+++ bookListAPI-DRF-Intro/myapp/urls.py
@@ -1,8 +1,13 @@
 from . import views
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('menu', views.menu),
-    path('menu_items', views.menu_items),
+    path('secret/', views.secret),
     path('item/<int:id>', views.item),
+    path('menu_items', views.menu_items),
+    path('token-auth/', obtain_auth_token),
+    path('manager-view', views.manager_view),
     path('category/<int:pk>',views.category_detail, name='category-detail'),
 ]
--- bookListAPI-DRF-Intro/myapp/views.py
+++ bookListAPI-DRF-Intro/myapp/views.py
@@ -1,22 +1,32 @@
 from .models import Menu, Category
+from .serializers import MenuSerializer, CategorySerializer
+
+
 from rest_framework import status
 from rest_framework.response import Response
-from django.shortcuts import render, get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import TemplateHTMLRenderer
-from .serializers import MenuSerializer, CategorySerializer
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.decorators import api_view, renderer_classes, permission_classes
 
+
+from django.core.paginator import Paginator, EmptyPage
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 @api_view(['GET', 'POST'])
 def menu(request):
     if request.method == 'GET':
         menu = Menu.objects.select_related('category').all()
+
+        # Query Params
         category_name = request.query_params.get('category')
         to_price = request.query_params.get('to_price')
         search = request.query_params.get('search')
         ordering = request.query_params.get('ordering')
+        perpage = request.query_params.get('perpage', default=2)
+        page = request.query_params.get('page', default=1)
 
+        # Filter and Search
         if category_name:
             menu = menu.filter(category__title=category_name)
 
@@ -27,7 +37,16 @@ def menu(request):
             menu = menu.filter(title__icontains=search)
 
         if ordering:
-            menu = menu.order_by(ordering)
+            ordering_fields = ordering.split(",")
+            menu = menu.order_by(*ordering_fields)
+
+        # Pagination
+        paginator = Paginator(menu, per_page=perpage)
+        try:
+            menu = paginator.page(number=page)
+        except EmptyPage:
+            menu = []
+
 
         serialized_menu = MenuSerializer(menu, many=True, context={'request':request})
         return Response(serialized_menu.data)
@@ -60,3 +79,18 @@ def menu_items(request):
     items = Menu.objects.select_related('category').all()
     ser_item = MenuSerializer(items, many=True)
     return Response({'data':ser_item.data}, template_name='menu-items.html')
+
+@api_view()
+@permission_classes([IsAuthenticated])
+def secret(request):
+    return Response({"message": "Some secret message"})
+
+
+# Working with user groups
+@api_view()
+@permission_classes([IsAuthenticated])
+def manager_view(request):
+    if request.user.groups.filter(name='Manager').exists():
+        return Response({"message":"Only Manager can see this"})
+    else:
+        return Response({"message":"You are not authorized"}, 403)
